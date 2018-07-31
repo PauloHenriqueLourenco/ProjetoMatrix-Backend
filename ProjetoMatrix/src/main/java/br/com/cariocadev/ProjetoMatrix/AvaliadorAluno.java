b0644f0ce5f6c10f6f354f77f4735b1537213fcc
Paulo Lourenço <paulohenrilourenco@gmail.com>
2018-07-31 17:23:27 -0300

    Melhorias no código

--- ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/AvaliadorAluno.java
+++ ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/AvaliadorAluno.java
@@ -1,69 +1,57 @@
 package br.com.cariocadev.ProjetoMatrix;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class AvaliadorAluno {
 
-	public BigDecimal getMaiorNota(BigDecimal[] notas) {
+	public boolean validarNotas(BigDecimal... notas) {
 		
-		if(notas == null) {
+		for (BigDecimal nota : notas)
+			if (validarNota(nota) == false)
+				return false;
+
+		return true;
+	}
+
+	public boolean validarNota(BigDecimal nota) {
+		return nota != null && nota.compareTo(BigDecimal.ZERO) >= 0 && nota.compareTo(BigDecimal.TEN) <= 0;
+	}
+
+	public BigDecimal getMaiorNota(BigDecimal[] notas) {
+
+		if (notas == null || notas.length == 0)
 			throw new IllegalArgumentException();
-		} else {
-			BigDecimal maiorNota = notas[0];
-			
-			for(int i = 1; i < notas.length; i++) {
-				if(notas[i].compareTo(maiorNota) > 0) {
-					maiorNota = notas[i];
-				}
-			}
-			
-			return maiorNota;
-		}		
+
+		Collections.sort(Arrays.asList(notas), Collections.reverseOrder());
+
+		return notas[0];
 	}
 
 	public BigDecimal getMedia(BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
-		
-		if(nota1 == null || nota2 == null || nota3 == null) {
+
+		if (validarNotas(nota1, nota2, nota3) == false)
 			throw new IllegalArgumentException();
-		} else {
-			
-			if(nota1.compareTo(BigDecimal.ZERO) < 0 || nota1.compareTo(BigDecimal.TEN) > 0 ||
-				nota2.compareTo(BigDecimal.ZERO) < 0 || nota2.compareTo(BigDecimal.TEN) > 0 ||
-				nota3.compareTo(BigDecimal.ZERO) < 0 || nota3.compareTo(BigDecimal.TEN) > 0) {
-				throw new IllegalArgumentException();
-			} else {
-			
-				BigDecimal somaNotas = nota1.add(nota2.add(nota3));
-				somaNotas = somaNotas.setScale(2, BigDecimal.ROUND_HALF_UP);
-				
-				BigDecimal media = somaNotas.divide(new BigDecimal("3"), BigDecimal.ROUND_HALF_UP);
-				
-				media = media.setScale(2, BigDecimal.ROUND_HALF_UP);
-			
-				return media;
-			}
-		}
+
+		BigDecimal somaDasNotas = nota1.add(nota2.add(nota3));
+
+		return somaDasNotas.divide(BigDecimal.valueOf(3), BigDecimal.ROUND_HALF_UP);
 	}
 
 	public String getStatus(BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
-		
-		String status;
-		
+
 		BigDecimal media = getMedia(nota1, nota2, nota3);
-		
-		if (media.compareTo(new BigDecimal("6.00")) >= 0) {
+
+		String status;
+
+		if (media.compareTo(BigDecimal.valueOf(6.0)) >= 0)
 			status = "APROVADO";
-		} else {
-			if (media.compareTo(new BigDecimal("4.00")) >= 0) {
-				status = "PROVA_FINAL";
-			} else {
-				status = "REPROVADO";
-			}
-		}
-		
+		else if (media.compareTo(BigDecimal.valueOf(4.0)) >= 0)
+			status = "PROVA_FINAL";
+		else
+			status = "REPROVADO";
+
 		return status;
 	}
-	
-	
-
 }
--- ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/CalculadoraSalario.java
+++ ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/CalculadoraSalario.java
@@ -1,18 +1,33 @@
 package br.com.cariocadev.ProjetoMatrix;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 public class CalculadoraSalario {
+
+	public List<faixaSalarialINNS> tabelaINSS = Arrays.asList(
+			new faixaSalarialINNS(BigDecimal.ZERO, BigDecimal.valueOf(1693.72), BigDecimal.valueOf(8.0)),
+			new faixaSalarialINNS(BigDecimal.valueOf(1693.73), BigDecimal.valueOf(2822.90), BigDecimal.valueOf(9.0)),
+			new faixaSalarialINNS(BigDecimal.valueOf(2822.91), BigDecimal.valueOf(Double.MAX_VALUE),
+					BigDecimal.valueOf(11.0)));
+
+	public List<faixaEtariaPlanoDeSaude> tabelaPlanoDeSaude = Arrays.asList(
+			new faixaEtariaPlanoDeSaude(0, 9, BigDecimal.valueOf(75.00)),
+			new faixaEtariaPlanoDeSaude(10, 19, BigDecimal.valueOf(112.50)),
+			new faixaEtariaPlanoDeSaude(20, 29, BigDecimal.valueOf(168.75)),
+			new faixaEtariaPlanoDeSaude(30, 39, BigDecimal.valueOf(253.13)),
+			new faixaEtariaPlanoDeSaude(40, 49, BigDecimal.valueOf(379.69)),
+			new faixaEtariaPlanoDeSaude(50, 59, BigDecimal.valueOf(569.54)),
+			new faixaEtariaPlanoDeSaude(60, Integer.MAX_VALUE, BigDecimal.valueOf(854.30)));
 
 	public BigDecimal getSalarioLiquido(BigDecimal salarioBruto, BigDecimal percentualImpostoINSS) {
 
 		if (salarioBruto == null || percentualImpostoINSS == null)
 			throw new IllegalArgumentException();
 
-		BigDecimal salarioLiquido = salarioBruto.subtract(
-				salarioBruto.multiply(percentualImpostoINSS).divide(new BigDecimal("100"), BigDecimal.ROUND_HALF_UP));
-
-		return salarioLiquido.setScale(2, BigDecimal.ROUND_HALF_UP);
+		return salarioBruto.subtract(salarioBruto.multiply(percentualImpostoINSS).divide(BigDecimal.valueOf(100.0),
+				BigDecimal.ROUND_HALF_UP)).setScale(2, BigDecimal.ROUND_HALF_UP);
 	}
 
 	public BigDecimal getValorINSS(BigDecimal salarioBruto) {
@@ -20,55 +35,95 @@ public class CalculadoraSalario {
 		if (salarioBruto == null)
 			throw new IllegalArgumentException();
 
-		BigDecimal percentualINSS = BigDecimal.ZERO;
+		BigDecimal percentualINSS = tabelaINSS.stream()
+				.filter(t -> salarioBruto.compareTo(t.getSalarioInicial()) >= 0
+						&& salarioBruto.compareTo(t.getSalarioFinal()) <= 0)
+				.map(faixaSalarialINNS::getPercentualINSS).findAny().orElse(null);
 
-		if (salarioBruto.compareTo(new BigDecimal("1693.72")) <= 0) {
-			percentualINSS = new BigDecimal("8.0");
-		} else {
-			if (salarioBruto.compareTo(new BigDecimal("2822.90")) <= 0) {
-				percentualINSS = new BigDecimal("9.0");
-			} else {
-				percentualINSS = new BigDecimal("11.0");
-			}
-		}
-
-		BigDecimal valorINSS = salarioBruto.multiply(percentualINSS).divide(new BigDecimal("100"),
-				BigDecimal.ROUND_HALF_UP);
-
-		return valorINSS.setScale(2, BigDecimal.ROUND_HALF_UP);
+		return salarioBruto.multiply(percentualINSS).divide(BigDecimal.valueOf(100), BigDecimal.ROUND_HALF_UP)
+				.setScale(2, BigDecimal.ROUND_HALF_UP);
 	}
 
 	public BigDecimal getValorPlanoDeSaude(Integer idade) {
 
-		BigDecimal valorPlanoDeSaude = BigDecimal.ZERO;
-
-		if (idade <= 9) {
-			valorPlanoDeSaude = new BigDecimal("75.00");
-		} else {
-			if (idade <= 19) {
-				valorPlanoDeSaude = new BigDecimal("112.50");
-			} else {
-				if (idade <= 29) {
-					valorPlanoDeSaude = new BigDecimal("168.75");
-				} else {
-					if (idade <= 39) {
-						valorPlanoDeSaude = new BigDecimal("253.13");
-					} else {
-						if (idade <= 49) {
-							valorPlanoDeSaude = new BigDecimal("379.69");
-						} else {
-							if (idade <= 59) {
-								valorPlanoDeSaude = new BigDecimal("569.54");
-							} else {
-								valorPlanoDeSaude = new BigDecimal("854.30");
-							}
-						}
-					}
-				}
-			}
-		}
+		BigDecimal valorPlanoDeSaude = tabelaPlanoDeSaude.stream()
+				.filter(t -> idade >= t.getIdadeInicial() && idade <= t.getIdadeFinal())
+				.map(faixaEtariaPlanoDeSaude::getPreco).findAny().orElse(null);
 
 		return valorPlanoDeSaude.setScale(2, BigDecimal.ROUND_HALF_UP);
 	}
 
+	class faixaSalarialINNS {
+
+		private BigDecimal salarioInicial;
+		private BigDecimal salarioFinal;
+		private BigDecimal percentualINSS;
+
+		public faixaSalarialINNS(BigDecimal salarioInicial, BigDecimal salarioFinal, BigDecimal percentualINSS) {
+			this.salarioInicial = salarioInicial;
+			this.salarioFinal = salarioFinal;
+			this.percentualINSS = percentualINSS;
+		}
+
+		public BigDecimal getSalarioInicial() {
+			return salarioInicial;
+		}
+
+		public void setSalarioInicial(BigDecimal salarioInicial) {
+			this.salarioInicial = salarioInicial;
+		}
+
+		public BigDecimal getSalarioFinal() {
+			return salarioFinal;
+		}
+
+		public void setSalarioFinal(BigDecimal salarioFinal) {
+			this.salarioFinal = salarioFinal;
+		}
+
+		public BigDecimal getPercentualINSS() {
+			return percentualINSS;
+		}
+
+		public void setPercentualINSS(BigDecimal percentualINSS) {
+			this.percentualINSS = percentualINSS;
+		}
+	}
+
+	class faixaEtariaPlanoDeSaude {
+
+		private int idadeInicial;
+		private int idadeFinal;
+		private BigDecimal preco;
+
+		public faixaEtariaPlanoDeSaude(int idadeInicial, int idadeFinal, BigDecimal preco) {
+			this.idadeInicial = idadeInicial;
+			this.idadeFinal = idadeFinal;
+			this.preco = preco;
+		}
+
+		public int getIdadeInicial() {
+			return idadeInicial;
+		}
+
+		public void setIdadeInicial(int idadeInicial) {
+			this.idadeInicial = idadeInicial;
+		}
+
+		public int getIdadeFinal() {
+			return idadeFinal;
+		}
+
+		public void setIdadeFinal(int idadeFinal) {
+			this.idadeFinal = idadeFinal;
+		}
+
+		public BigDecimal getPreco() {
+			return preco;
+		}
+
+		public void setPreco(BigDecimal preco) {
+			this.preco = preco;
+		}
+	}
 }
